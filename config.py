0207bfbe22757d99cd24bc9581cce7ad3aa2a05d
Jeff <jeffin@antonal.com>
2020-05-08 10:30:39 +0530

    config prechecks added

--- config.py
+++ config.py
@@ -2,3 +2,17 @@ conf = {
     'profiles':['dev','stag','pre-prod','prod'],
     'tag':'PROJECT',
 }
+
+
+
+# conf pre checks
+if not isinstance(conf['profiles'], list) and len(conf['profiles']) < 1:
+    print 'Please add profiles as non empty list..'
+    print 'Exiting...'
+    exit()
+
+
+if not isinstance(conf['tag'], str) and len(conf['tag']) < 1:
+    print 'Please provide tag value as a non empty string.'
+    print 'Exiting...'
+    exit()
